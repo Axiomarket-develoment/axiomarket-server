6ba0b23c7ac2fcc561ac8a1becfccc525d69d136
danieldaudu65 <danieldaudu65@gmail.com>
2026-04-28 11:03:36 +0100

    market generation

--- crons/marketCron.js
+++ crons/marketCron.js
@@ -14,7 +14,7 @@ function startMarketCron() {
 
   // 🔵 15 MINUTE MARKETS
  // 🕘 DAILY MARKET GENERATION (9 AM)
-cron.schedule("53 10 * * *", async () => {
+cron.schedule("07 11 * * *", async () => {
   console.log("🌅 Generating markets...");
   await generateMarkets();
 }, {
